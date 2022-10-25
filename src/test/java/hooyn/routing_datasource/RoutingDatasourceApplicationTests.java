00cfbd3fe2a74b546220d667196cf9d8b7ce7213
JungHoyun <hooyn_industry@naver.com>
2022-10-25 15:26:17 +0900

    Initial Commit

---
+++ src/test/java/hooyn/routing_datasource/RoutingDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package hooyn.routing_datasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingDatasourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
